fc44052137ad63f80c49e7b46b434dac8666cfb9
A-Nagy <70291196+A-Nagy@users.noreply.github.com>
2026-07-26 22:03:00 +0300

    Day4

--- Csharp_4/Day_4/DashBoard_Update_11.cs
+++ Csharp_4/Day_4/DashBoard_Update_11.cs
@@ -10,195 +10,202 @@ namespace Csharp_4.Day_4
     {     
         static void Main(string[] args) 
           {
-              Console.WriteLine("Chosse the Number Of System You Need To Use 1-4");
-              Console.WriteLine(" 1.Employee Salary Information \n" +
-                                " 2.Student Percentage Calculator \n" +
-                                " 3.Patient BMI Calculator \n" +
-                                " 4.Count of Days in Months \n" +
-                                " 5.Exit ");
-              int ch = Convert.ToInt32(Console.ReadLine());
-
-            switch (ch) 
+            int ch = 0;
+            do
             {
-                case 1:
-                    Console.WriteLine("=================================");
-                    Console.WriteLine("   Employee Salary Information   ");
-                    Console.WriteLine("=================================");
+                Console.WriteLine();
+                Console.WriteLine("Chosse the Number Of System You Need To Use 1-4");
+                Console.WriteLine(" 1.Employee Salary Information \n" +
+                                  " 2.Student Percentage Calculator \n" +
+                                  " 3.Patient BMI Calculator \n" +
+                                  " 4.Count of Days in Months \n" +
+                                  " 5.Exit ");
+                ch = Convert.ToInt32(Console.ReadLine());
 
-                    // declare variables
-                    string employeeName;
-        double monthlySalary;
-        double annualSalary;
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("=================================");
+                        Console.WriteLine("   Employee Salary Information   ");
+                        Console.WriteLine("=================================");
 
-        //input from the user
-        Console.Write("Enter your name: ");
-                    employeeName = Console.ReadLine();
-                    Console.Write("Enter your monthly salary: ");
-                    monthlySalary = Convert.ToDouble(Console.ReadLine());
+                        // declare variables
+                        string employeeName;
+                        double monthlySalary;
+                        double annualSalary;
 
-                    //calculate annual salary
-                    annualSalary = monthlySalary* 12;
+                        //input from the user
+                        Console.Write("Enter your name: ");
+                        employeeName = Console.ReadLine();
+                        Console.Write("Enter your monthly salary: ");
+                        monthlySalary = Convert.ToDouble(Console.ReadLine());
 
-                    //Display the results
+                        //calculate annual salary
+                        annualSalary = monthlySalary * 12;
 
-                    Console.WriteLine("Employee Name: " + employeeName);
-                    Console.WriteLine("Monthly Salary: " + monthlySalary);
-                    Console.WriteLine("Annual Salary: " + annualSalary);
-                    if (monthlySalary >= 15000)
-                    {
-                        Console.WriteLine("High Salary");
-                    }
-                    else if (monthlySalary >= 10000)
-                    {
-                        Console.WriteLine("Normal Salary");
-                    }
-                    else if (monthlySalary >= 5000)
-{
-    Console.WriteLine("low Salary");
-}
-else
-{
-    Console.WriteLine("very low Salary");
-}
+                        //Display the results
 
-
-break;
-                case 2:
-    Console.WriteLine("=================================");
-    Console.WriteLine("   Student Percentage Calculator ");
-    Console.WriteLine("=================================");
-    // declare variables
-    string studentName;
-    double obtainedMark;
-    double fullMark;
-    double percentage;
-
-    //input from the user
-    Console.Write("Enter your name: ");
-    studentName = Console.ReadLine();
-    Console.Write("Enter your obtained mark: ");
-    obtainedMark = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Enter the full mark: ");
-    fullMark = Convert.ToDouble(Console.ReadLine());
-
-    //calculate percentage
-    percentage = (obtainedMark / fullMark) * 100;
-
-    //Display the results
-
-    Console.WriteLine("Student Name: " + studentName);
-    Console.WriteLine("Obtained Mark: " + obtainedMark);
-    Console.WriteLine("Full Mark: " + fullMark);
-    Console.WriteLine("Percentage: " + percentage + "%");
-    if (percentage >= 85)
-    {
-        Console.WriteLine("Excellent");
-    }
-    else if (percentage >= 75)
-    {
-        Console.WriteLine("very good");
-    }
-    else if (percentage >= 65)
-    {
-        Console.WriteLine("good");
-    }
-    else if (percentage >= 50)
-    {
-        Console.WriteLine("pass");
-    }
-    else
-    {
-        Console.WriteLine("faild");
-    }
+                        Console.WriteLine("Employee Name: " + employeeName);
+                        Console.WriteLine("Monthly Salary: " + monthlySalary);
+                        Console.WriteLine("Annual Salary: " + annualSalary);
+                        if (monthlySalary >= 15000)
+                        {
+                            Console.WriteLine("High Salary");
+                        }
+                        else if (monthlySalary >= 10000)
+                        {
+                            Console.WriteLine("Normal Salary");
+                        }
+                        else if (monthlySalary >= 5000)
+                        {
+                            Console.WriteLine("low Salary");
+                        }
+                        else
+                        {
+                            Console.WriteLine("very low Salary");
+                        }
 
 
-    break;
-                case 3:
-                    Console.WriteLine("=================================");
-                    Console.WriteLine("   Patient BMI Calculator ");
-                    Console.WriteLine("=================================");
+                        break;
+                    case 2:
+                        Console.WriteLine("=================================");
+                        Console.WriteLine("   Student Percentage Calculator ");
+                        Console.WriteLine("=================================");
+                        // declare variables
+                        string studentName;
+                        double obtainedMark;
+                        double fullMark;
+                        double percentage;
 
-                    //Declear Varaible 
-                    string PatientName;
-                    double height;
-                    double weight;
-                    double BMI;
-                    //inputs from user 
-                    Console.WriteLine("pls enter your name ");
-                    PatientName = Console.ReadLine();
-                    Console.WriteLine("pls enter your height");
-                    height = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("pls enter your weight ");
-                    weight = Convert.ToDouble(Console.ReadLine());
-                    //calcualte the Bmi  
-                    BMI = weight / (height * height);
-                    //Display Result 
-                    Console.WriteLine("your name is : " + PatientName);
-                    Console.WriteLine("your height is " + height + " M");
-                    Console.WriteLine("your weight is " + weight + " Kg");
-                    Console.WriteLine("Bmi Is : " + BMI);
+                        //input from the user
+                        Console.Write("Enter your name: ");
+                        studentName = Console.ReadLine();
+                        Console.Write("Enter your obtained mark: ");
+                        obtainedMark = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter the full mark: ");
+                        fullMark = Convert.ToDouble(Console.ReadLine());
 
-                    if (BMI < 18.5)
-                    {
-                        Console.WriteLine("Underweight");
-                    }
-                    else if (BMI >= 18.5 && BMI < 25)
-                    {
-                        Console.WriteLine("Normal Weight");
+                        //calculate percentage
+                        percentage = (obtainedMark / fullMark) * 100;
 
-                    }
-                    else if (BMI < 30)
-                    {
-                        Console.WriteLine("Overweight");
+                        //Display the results
 
-                    }
-                    else if (BMI >= 30)
-                    {
-                        Console.WriteLine("Obese");
+                        Console.WriteLine("Student Name: " + studentName);
+                        Console.WriteLine("Obtained Mark: " + obtainedMark);
+                        Console.WriteLine("Full Mark: " + fullMark);
+                        Console.WriteLine("Percentage: " + percentage + "%");
+                        if (percentage >= 85)
+                        {
+                            Console.WriteLine("Excellent");
+                        }
+                        else if (percentage >= 75)
+                        {
+                            Console.WriteLine("very good");
+                        }
+                        else if (percentage >= 65)
+                        {
+                            Console.WriteLine("good");
+                        }
+                        else if (percentage >= 50)
+                        {
+                            Console.WriteLine("pass");
+                        }
+                        else
+                        {
+                            Console.WriteLine("faild");
+                        }
 
-                    }
 
-                    break;
-                case 4:
-                    Console.WriteLine("=================================");
-                    Console.WriteLine("   Count of Days in Months ");
-                    Console.WriteLine("=================================");
+                        break;
+                    case 3:
+                        Console.WriteLine("=================================");
+                        Console.WriteLine("   Patient BMI Calculator ");
+                        Console.WriteLine("=================================");
 
-                    Console.WriteLine("pls Enter Month Number 1 to 12");
-                    int N_Nuber = Convert.ToInt32(Console.ReadLine());
+                        //Declear Varaible 
+                        string PatientName;
+                        double height;
+                        double weight;
+                        double BMI;
+                        //inputs from user 
+                        Console.WriteLine("pls enter your name ");
+                        PatientName = Console.ReadLine();
+                        Console.WriteLine("pls enter your height");
+                        height = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("pls enter your weight ");
+                        weight = Convert.ToDouble(Console.ReadLine());
+                        //calcualte the Bmi  
+                        BMI = weight / (height * height);
+                        //Display Result 
+                        Console.WriteLine("your name is : " + PatientName);
+                        Console.WriteLine("your height is " + height + " M");
+                        Console.WriteLine("your weight is " + weight + " Kg");
+                        Console.WriteLine("Bmi Is : " + BMI);
 
-                    switch (N_Nuber)
-                    {
-                        case 1:
-                        case 3:
-                        case 5:
-                        case 7:
-                        case 8:
-                        case 10:
-                        case 12:
-                            Console.WriteLine("Count Of Days is 31");
-                            break;
-                        case 4:
-                        case 6:
-                        case 9:
-                        case 11:
-                            Console.WriteLine("Count Of Days is 30");
-                            break;
-                        case 2:
-                            Console.WriteLine("Count Of Days is 31");
-                            break;
-                        default:
-                            Console.WriteLine("invalid Number");
-                            break;
+                        if (BMI < 18.5)
+                        {
+                            Console.WriteLine("Underweight");
+                        }
+                        else if (BMI >= 18.5 && BMI < 25)
+                        {
+                            Console.WriteLine("Normal Weight");
 
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Invalid Number");
-                    break;
+                        }
+                        else if (BMI < 30)
+                        {
+                            Console.WriteLine("Overweight");
 
-            }
+                        }
+                        else if (BMI >= 30)
+                        {
+                            Console.WriteLine("Obese");
 
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("=================================");
+                        Console.WriteLine("   Count of Days in Months ");
+                        Console.WriteLine("=================================");
+
+                        Console.WriteLine("pls Enter Month Number 1 to 12");
+                        int N_Nuber = Convert.ToInt32(Console.ReadLine());
+
+                        switch (N_Nuber)
+                        {
+                            case 1:
+                            case 3:
+                            case 5:
+                            case 7:
+                            case 8:
+                            case 10:
+                            case 12:
+                                Console.WriteLine("Count Of Days is 31");
+                                break;
+                            case 4:
+                            case 6:
+                            case 9:
+                            case 11:
+                                Console.WriteLine("Count Of Days is 30");
+                                break;
+                            case 2:
+                                Console.WriteLine("Count Of Days is 31");
+                                break;
+                            default:
+                                Console.WriteLine("invalid Number");
+                                break;
+
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Exit...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Number");
+                        break;
+
+                }
+            } while (ch != 5);
 
 
         }
