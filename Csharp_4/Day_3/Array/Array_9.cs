143e13456181fe3608cb2f7d8adfc339b7cc7b8f
A-Nagy <70291196+A-Nagy@users.noreply.github.com>
2026-07-26 21:50:03 +0300

    NextUpdate

--- Csharp_4/Day_3/Array/Array_9.cs
+++ Csharp_4/Day_3/Array/Array_9.cs
@@ -9,63 +9,63 @@ namespace Csharp_4.Day_3.Array
 {
      class Array_9
     {
-        static void Main(string[] args)
-        {
+//        static void Main(string[] args)
+//        {
  
 
 
-            /*
+//            /*
              
-            ### ✅ Task 1: Print First and Last Item in Array
-            Write a program that creates a string array with 5 
-                        country names and prints the first and last country.
-             */
+//            ### ✅ Task 1: Print First and Last Item in Array
+//            Write a program that creates a string array with 5 
+//                        country names and prints the first and last country.
+//             */
 
-            //string[] countries = { "Egypt" , "Saudi Arabia" , "Moraco", "canda" , "isbain"};
-            //Console.WriteLine($"First Country is {countries[0]} \n  Last Country is : {countries[countries.Length-1]}");
+//            //string[] countries = { "Egypt" , "Saudi Arabia" , "Moraco", "canda" , "isbain"};
+//            //Console.WriteLine($"First Country is {countries[0]} \n  Last Country is : {countries[countries.Length-1]}");
 
-            /*
-             ### ✅ Task 2: Swap Two Elements in Array
-Create an array of 3 integers. Swap the first and last elements, then print the array.
+//            /*
+//             ### ✅ Task 2: Swap Two Elements in Array
+//Create an array of 3 integers. Swap the first and last elements, then print the array.
              
-            // */
-            //int[] numbers = { 10, 20, 30 };
+//            // */
+//            //int[] numbers = { 10, 20, 30 };
 
-            //// swap by variable 
+//            //// swap by variable 
 
-            //int x = numbers[0];
-            //numbers[0] = numbers[2];
-            //numbers[2] = x; 
+//            //int x = numbers[0];
+//            //numbers[0] = numbers[2];
+//            //numbers[2] = x; 
             
-            //Console.WriteLine("First Item " + numbers[0]);
-            //Console.WriteLine("secand Item " + numbers[1]);
-            //Console.WriteLine("third Item " + numbers[2]);
+//            //Console.WriteLine("First Item " + numbers[0]);
+//            //Console.WriteLine("secand Item " + numbers[1]);
+//            //Console.WriteLine("third Item " + numbers[2]);
 
 
-            /*
-### ✅ Task 3: Calculate Average of 4 Grades
-Create an array of 4 integers representing grades. 
-            Calculate and print the average.
-            */
-            //int[] grades = { 90, 80, 85, 95 };
-            //double avrage = (grades[0] + grades[1] + grades[2] + grades[3]) / (grades.Length);
-            //Console.WriteLine("Avrage Grade is " + avrage);
+//            /*
+//### ✅ Task 3: Calculate Average of 4 Grades
+//Create an array of 4 integers representing grades. 
+//            Calculate and print the average.
+//            */
+//            //int[] grades = { 90, 80, 85, 95 };
+//            //double avrage = (grades[0] + grades[1] + grades[2] + grades[3]) / (grades.Length);
+//            //Console.WriteLine("Avrage Grade is " + avrage);
 
-            /*
+//            /*
              
-### ✅ Task 4: Replace a Value in Array
-            Create a string array with 3 names.
-            Replace the second name with a new one and display all names.
+//### ✅ Task 4: Replace a Value in Array
+//            Create a string array with 3 names.
+//            Replace the second name with a new one and display all names.
              
-             */
-            string[] names = { "ahmad ", "sara ", "Ali" };
-            names[1] = "Omar";
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
+//             */
+//            string[] names = { "ahmad ", "sara ", "Ali" };
+//            names[1] = "Omar";
+//            Console.WriteLine(names[0]);
+//            Console.WriteLine(names[1]);
+//            Console.WriteLine(names[2]);
 
 
 
-        }
+//        }
     }
 }
---
+++ Csharp_4/Day_4/Loops_10.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_4.Day_4
+{
+    class Loops_10
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Console.WriteLine("1");
+        //    //Console.WriteLine("2");
+        //    //Console.WriteLine("3");
+        //    //Console.WriteLine("4");
+        //    //Console.WriteLine("5");
+        //    //Console.WriteLine("6");
+        //    //Console.WriteLine("7");
+        //    //Console.WriteLine("8");
+        //    //Console.WriteLine("9");
+        //    //Console.WriteLine("10");
+
+
+        //    //1. for 
+
+        //    //       //counter = 1  //condition       // step 
+        //    //for (int Counter = 1; Counter <= 1000; Counter++) 
+        //    //{
+        //    //    Console.WriteLine(Counter); //Code in Loop
+        //    //}
+        //    ///*### `for` loop – Counting from 1 to 50  
+        //    //Iterates using a counter, useful when the number of repetitions is known.
+
+        //    // */
+        //    //for (int x = 1; x <= 50; x++)
+        //    //{
+        //    //    Console.WriteLine(x);
+        //    //}
+
+        //    /////////////////////////////////////
+
+        //    //2.while 
+
+        //    //string username = "";
+
+        //    //while (username != "admin")
+        //    //{
+        //    //    Console.WriteLine("Pls Enter UserName To Login ");
+        //    //    username = Console.ReadLine();
+        //    //}
+        //    //    Console.WriteLine("Welcome , Admin");
+
+        //    //int Counter = 1;
+
+        //    //while (Counter <= 10) 
+        //    //{
+
+        //    //    Console.WriteLine(Counter);
+        //    //    Counter ++ ;
+        //    //}
+
+
+        //    //3.do... while 
+
+        //    //int Counter = 1;
+        //    //do
+        //    //{
+
+        //    //    Console.WriteLine(Counter);
+        //    //    Counter++;
+        //    //} while (Counter >= 10);
+
+        //    /*
+             
+        //    ## ✅ Task 1: Print Numbers from 1 to 10 using a `for` loop  
+        //    Print numbers from 1 to 10 on the console using a `for` loop.
+        //     */
+        //    //for (int i = 1; i <= 10; i++)
+        //    //{
+        //    //    Console.WriteLine(i);
+        //    //}
+
+        //    /*
+        //     * ## ✅ Task 2: Sum of Even Numbers using `while` loop  
+        //    Ask the user to enter a number `n`, 10 =>  n % 2 ==0 , 2 4 6 8 10 
+        //    then calculate the sum of all even numbers from 1 to `n`.
+        //     */
+        //    //Console.WriteLine("Plz Enter Number ");
+        //    //int n = int.Parse(Console.ReadLine());//10
+        //    //int start = 1;
+        //    //int sum = 0;
+
+        //    //Console.Write("N" + "\t");
+        //    //Console.Write("start" + "\t");
+        //    //Console.Write("sum" + "\t");
+        //    //Console.WriteLine();
+        //    //while (start <= n)
+        //    //{
+        //    //    if (start % 2 == 0)
+        //    //    {
+        //    //        sum += start;
+        //    //    }
+        //    //    start++;
+        //    //    Console.Write(n + "\t");
+        //    //    Console.Write(start + "\t");
+        //    //    Console.Write(sum + "\t");
+        //    //    Console.WriteLine();
+
+        //    //}
+
+        //    //Console.WriteLine($"Final Result : Sum of Even Number From 1 to {n} is {sum}");
+
+        //    /*
+        //     * 
+        //        ## ✅ Task 3: Password Retry using `do-while` loop  
+        //        Let the user enter a password and keep prompting
+        //        until they enter "admin123".
+        //     */
+
+        //    //    string password;
+
+        //    //    do
+        //    //    {
+        //    //        Console.Write("Enter password: ");
+        //    //        password = Console.ReadLine();
+
+        //    //    } while (password != "admin123");
+
+        //    //    Console.WriteLine("Access granted!");
+        //    //
+
+        //    string[] names = { "ahmad ", "sara ", "Ali" , "ahmad2 ", "sara2 ", "Ali2" };//6
+
+        //    for (int i = 0; i < names.Length; i++) 
+        //    {
+        //        Console.WriteLine(names[i]);
+        //    }
+
+
+        //    //4.Froeach 
+        //    foreach (string name in names) 
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //}
+
+    }
+}
